8ac8092680d04b6b90cdb3c1f918a025fecc4d9c
Rodrigo Stevaux <ro.stevaux@gmail.com>
2017-04-20 19:28:23 -0300

    Carregar interpretador e rodar script

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+int main(int argc, const char *argv[])
+{
+	lua_State *L = luaL_newstate();
+
+	luaL_openlibs(L);
+
+	luaL_dofile(L, "root/main.lua");
+	
+	lua_close(L);
+
+}
+
